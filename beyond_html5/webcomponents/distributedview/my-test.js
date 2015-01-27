f7befa385d58a3d9b240358410de24b84ee46d04
crdlc <crdlc@tid.es>
2015-01-27 12:42:31 +0100

    test 3

--- beyond_html5/webcomponents/distributedview/my-test.js
+++ beyond_html5/webcomponents/distributedview/my-test.js
@@ -1,10 +1,12 @@
 'use strict';
 
 (function(exports) {
+
   exports.MyTest = {
     test: function() {
-      console.log('form', document.currentScript.ownerDocument.querySelector('form-test'));
-      console.log('list: ', document.currentScript.ownerDocument.querySelector('list-test'));
+      console.log('form:', document.currentScript.ownerDocument.querySelector('form-test'));
+      console.log('list:', document.currentScript.ownerDocument.querySelector('list-test'));
     }
   };
-}(this));
+
+}(window));
